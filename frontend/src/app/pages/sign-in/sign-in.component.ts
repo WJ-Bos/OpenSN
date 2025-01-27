e27f393d43e8eb907f172b52f819eb1d483183f2
WJbos <wjbosdev@gmail.com>
2025-01-27 20:24:45 +0200

    added imports

--- frontend/src/app/pages/sign-in/sign-in.component.ts
+++ frontend/src/app/pages/sign-in/sign-in.component.ts
@@ -1,8 +1,14 @@
 import { Component } from '@angular/core';
+import {MatCardModule} from '@angular/material/card';
+import {MatFormField} from '@angular/material/form-field';
+import {MatButtonModule} from '@angular/material/button';
 
 @Component({
   selector: 'app-sign-in',
-  imports: [],
+  imports: [MatCardModule,
+    MatFormField,
+    MatButtonModule,
+  ],
   templateUrl: './sign-in.component.html',
   styleUrl: './sign-in.component.css'
 })
