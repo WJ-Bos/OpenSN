da62403977c5900080e26bb7c1622c222c79ef1c
WJbos <wjbosdev@gmail.com>
2025-01-28 22:28:09 +0200

    Added background to sign in

--- frontend/src/app/pages/sign-in/sign-in.component.ts
+++ frontend/src/app/pages/sign-in/sign-in.component.ts
@@ -1,17 +1,23 @@
 import { Component } from '@angular/core';
 import {MatCardModule} from '@angular/material/card';
-import {MatFormField} from '@angular/material/form-field';
+import {MatFormField, MatLabel} from '@angular/material/form-field';
 import {MatButtonModule} from '@angular/material/button';
 
 @Component({
   selector: 'app-sign-in',
   imports: [MatCardModule,
     MatFormField,
-    MatButtonModule,
+    MatButtonModule, MatLabel,
   ],
   templateUrl: './sign-in.component.html',
   styleUrl: './sign-in.component.css'
 })
 export class SignInComponent {
+
+  backGroundImage = "";
+
+  get bgImage():string{
+    return this.backGroundImage;
+  }
 
 }
