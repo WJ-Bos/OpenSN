3d09a658f1cb096a03e7f8a78d91b484e440ec5a
WJbos <wjbosdev@gmail.com>
2025-01-28 13:30:34 +0200

    Added Tailwind css to the project

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {SignInComponent} from './pages/sign-in/sign-in.component';
+import {SignUpComponent} from './pages/sign-up/sign-up.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, SignInComponent, SignUpComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
